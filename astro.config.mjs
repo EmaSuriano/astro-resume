e03bf434ecbf5fb8900f519ba4c0415c52a6ca02
Marco Antonio Gonzalez Junior <m@rco.sh>
2024-10-19 12:19:52 -0300

    fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,18 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx';
+import starlight from '@astrojs/starlight';
+import { astroExpressiveCode } from '@astrojs/starlight/expressive-code';
 
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    astroExpressiveCode(),
+    mdx(), 
+    starlight({
+      title: '',
+    }),
+  ],
   vite: {
     ssr: {
       external: ['svgo'],
