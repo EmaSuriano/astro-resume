0712ada808fe9448beece502efc0261453b851e3
Marco Antonio Gonzalez Junior <m@rco.sh>
2024-10-19 12:23:54 -0300

    fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
-import starlight from '@astrojs/starlight';
-import { astroExpressiveCode } from '@astrojs/starlight/expressive-code';
 
 export default defineConfig({
   integrations: [
